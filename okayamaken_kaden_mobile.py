7eaa9ae8b3e2671c23b552978009ecfe1f524964
fuminishiki <136785393+fuminishiki@users.noreply.github.com>
2023-06-19 12:21:04 +0900

    Create okayamaken_kaden_mobile.py

---
+++ okayamaken_kaden_mobile.py
@@ -0,0 +1,135 @@
+import folium
+
+m = folium.Map(location=[34.8919956,133.8635534],zoom_start=9)
+m
+
+folium.Marker(location=[34.6755952,133.8792594],popup='エディオン 一宮店　　　　　　　　',icon=folium.Icon(color="blue")).add_to(m)
+m
+
+folium.Marker(location=[34.5817379,133.4394264],popup='エディオン 井原店　　　　　　　　',icon=folium.Icon(color="blue")).add_to(m)
+m
+
+folium.Marker(location=[34.5786898,133.7685085],popup='エディオン 倉敷本店　　　　　　　　　',icon=folium.Icon(color="blue")).add_to(m)
+m
+
+folium.Marker(location=[34.5554139,133.7457334],popup='エディオン 倉敷南店　　　　　　　　　',icon=folium.Icon(color="blue")).add_to(m)
+m
+
+folium.Marker(location=[34.6261772,133.9378637],popup='エディオン 岡南店　　　　　　　　',icon=folium.Icon(color="blue")).add_to(m)
+m
+
+folium.Marker(location=[34.6582312,134.0066575],popup='エディオン 西大寺店　　　　　　　　　',icon=folium.Icon(color="blue")).add_to(m)
+m
+
+folium.Marker(location=[34.7531567,134.0223453],popup='エディオン 山陽店　　　　　　　　',icon=folium.Icon(color="blue")).add_to(m)
+m
+
+folium.Marker(location=[34.6370997,133.9037496],popup='エディオン 下中野店　　　　　　　　　',icon=folium.Icon(color="blue")).add_to(m)
+m
+
+folium.Marker(location=[34.6818823,133.7697104],popup='エディオン 総社店　　　　　　　　',icon=folium.Icon(color="blue")).add_to(m)
+m
+
+folium.Marker(location=[34.4938075,133.9459444],popup='エディオン 玉野店　　　　　　　　',icon=folium.Icon(color="blue")).add_to(m)
+m
+
+folium.Marker(location=[35.0604457,134.0287746],popup='エディオン 津山本店　　　　　　　　　',icon=folium.Icon(color="blue")).add_to(m)
+m
+
+folium.Marker(location=[34.6224407,133.7938042],popup='エディオン 中庄店　　　　　　　　',icon=folium.Icon(color="blue")).add_to(m)
+m
+
+folium.Marker(location=[34.6753676,133.9449947],popup='エディオン 東川原店　　　　　　　　　',icon=folium.Icon(color="blue")).add_to(m)
+m
+
+folium.Marker(location=[34.7426114,134.1846803],popup='エディオン 備前店　　　　　　　　',icon=folium.Icon(color="blue")).add_to(m)
+m
+
+folium.Marker(location=[34.6383528,133.9051328],popup='ベスト電器岡山本店　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.7897131,134.0115197],popup='ヤマダデンキ テックランド赤磐店　　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.5885438,133.444506],popup='ヤマダデンキ テックランド井原店　　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.6306028,133.8939958],popup='ヤマダデンキ テックランド岡山本店　　　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.543157,133.7289038],popup='ヤマダデンキ テックランド倉敷神田店　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.5579312,133.6812976],popup='ヤマダデンキ テックランド倉敷玉島店　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.4621208,133.8064526],popup='ヤマダデンキ テックランド児島店　　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.6636708,134.0897586],popup='ヤマダデンキ テックランド瀬戸内店　　　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.6693181,133.751924],popup='ヤマダデンキ テックランド総社店　　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.7847987,133.6105182],popup='ヤマダデンキ テックランド高梁店　　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.4948112,133.9547056],popup='ヤマダデンキ テックランド玉野店　　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.9643447,133.4726855],popup='ヤマダデンキ テックランド新見店　　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[35.0250915,134.1624867],popup='ヤマダデンキ テックランド美作店　　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.6311274,133.932606],popup='ヤマダデンキ テックランドNew岡南店　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.5285677,133.593859],popup='ヤマダデンキ ヤマダアウトレット鴨方店　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[35.0655079,134.0494733],popup='ヤマダデンキ ヤマダアウトレット津山店　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.6772389,133.9823469],popup='YAMADA web.com東岡山店　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.6659324,133.9197975],popup='ビックカメラ 岡山駅前店　　　　　　　　　　　',icon=folium.Icon(color="lightred")).add_to(m)
+m
+
+folium.Marker(location=[34.6521064,134.028199],popup='ケーズデンキ 岡山西大寺店　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.6586332,133.8858855],popup='ケーズデンキ 岡山大安寺店　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.5921164,133.7526797],popup='ケーズデンキ 倉敷店　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.4669108,133.810218],popup='ケーズデンキ 児島店　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[35.0588351,134.027436],popup='ケーズデンキ 津山店　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.5926734,133.8890778],popup='ケーズデンキ 藤田店　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.6615374,133.9179560],popup='イオンモバイル 岡山店　　　　　　　　　　',icon=folium.Icon(color="purple")).add_to(m)
+m
+
+folium.Marker(location=[34.6020191,133.7458138],popup='イオンモバイル 倉敷店　　　　　　　　　　',icon=folium.Icon(color="purple")).add_to(m)
+m
+
+folium.Marker(location=[35.0649040,134.0457948],popup='イオンモバイル 津山店　　　　　　　　　　',icon=folium.Icon(color="purple")).add_to(m)
+m
+
+folium.Marker(location=[34.6370953,133.9086896],popup='PC DEPOT 岡山・下中野店　　　　　　　　　　　　',icon=folium.Icon(color="beige")).add_to(m)
+m
+
+folium.Marker(location=[34.6620461,133.9176842],popup='マーキュリー 岡山営業所　　　　　　　　　　　',icon=folium.Icon(color="white")).add_to(m)
+m
+
+m.save('okayamaken_kaden_mobile.html')
